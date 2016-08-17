973436f0d1b51f824769e6c856a3f4e8721bce1c
MatanSilver <matansilver@gmail.com>
2016-08-17 09:26:20 -0400

    Bump version: 0.4.0 → 0.4.1

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ test_requirements = [
 
 setup(
     name='wikicreator',
-    version='0.4.0',
+    version='0.4.1',
     description="Python Boilerplate contains all the boilerplate you need to create a Python package.",
     long_description=readme + '\n\n' + history,
     author="Matan Silver",
